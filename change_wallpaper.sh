eacfc99c5eaf5a66dcf1a29de1ee7bc91888ec50
Alexandre Lithaud <alexandre.ltd71@gmail.com>
2023-07-31 14:31:52 +0200

    Wallpaper scipt and some wallpapers

---
+++ change_wallpaper.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/zsh
+
+# Path to wallpaper folder
+wallpaper_folder="/home/alex/Documents/dev/OtherProjects/wallpaperModifier/wallpapers"
+
+# Get all wallpapers from the folder
+wallpaper=("${wallpaper_folder}"/*)
+
+# If the folder is empty then fail
+if [ ${#wallpapers[@]} -eq 0 ]; then
+  echo "Wallpaper folder is empty or does not exist."
+  exit 1
+fi
+
+# Use the $RANDOM to get a random picture from the folder
+selected_wallpaper="${wallpapers[$RANDOM % ${#wallpapers[@]}]}"
+
+# Use gsettings to change the wallpaper
+gsettings set org.gnome.desktop.background picture-uri "file://${selected_wallpaper}"
